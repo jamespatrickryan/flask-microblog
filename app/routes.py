c518fc6a43c58796bede090c9ce01710196964bb
James Patrick Ryan A. Pasaforte <jamespatrick.pasaforte@g.msuiit.edu.ph>
2022-12-12 00:24:26 +0800

    style: rename a variable

--- app/routes.py
+++ app/routes.py
@@ -41,10 +41,11 @@ def login():
             return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
+
+        next = request.args.get('next')
+        if not next or url_parse(next).netloc != '':
+            next = url_for('index')
+        return redirect(next)
 
     return render_template('login.html', title='Log In', form=form)
 
