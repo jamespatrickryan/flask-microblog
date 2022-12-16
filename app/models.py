ff6cbfdb5fb1aa21f2bbd28fb46c935c48702f38
James Patrick Ryan A. Pasaforte <jamespatrick.pasaforte@g.msuiit.edu.ph>
2022-12-16 21:44:19 +0800

    feat(user profile page and avatars): add a bio and last-seen fields

--- app/models.py
+++ app/models.py
@@ -15,6 +15,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    bio = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return f'<User {self.username}>'
