348803c41a99944cbc785e5d41443dbfa43c3c80
James Patrick Ryan A. Pasaforte <jamespatrick.pasaforte@g.msuiit.edu.ph>
2022-12-26 17:40:14 +0800

    feat(database v0.4): add a shell context processor function

--- microblog.py
+++ microblog.py
@@ -1,1 +1,12 @@
-from app import app
+from app import app, db
+
+from app.models import Post, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'Post': Post,
+        'User': User
+    }
